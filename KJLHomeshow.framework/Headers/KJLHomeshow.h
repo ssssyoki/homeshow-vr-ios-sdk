adecd1b0d7fd665d7b12c4d60488b9634171003f
tianyu <tianyu@qunhemail.com>
2020-07-31 11:21:18 +0800

    feat: update

---
+++ KJLHomeshow.framework/Headers/KJLHomeshow.h
@@ -0,0 +1,20 @@
+//
+//  KJLHomeshow.h
+//  KJLHomeshow
+//
+//  Created by xiangyun on 2020/4/1.
+//  Copyright © 2020 kujiale. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KJLHomeshow.
+FOUNDATION_EXPORT double KJLHomeshowVersionNumber;
+
+//! Project version string for KJLHomeshow.
+FOUNDATION_EXPORT const unsigned char KJLHomeshowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KJLHomeshow/PublicHeader.h>
+
+#import "WKWebViewJavascriptBridge.h"
+#import "WebViewJavascriptBridge.h"
